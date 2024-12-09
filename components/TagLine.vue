ce6a40fea224bb64410edfcddbb69ce415fbd7a8
WasitpolKuekkong <6531503072@lamduan.mfu.ac.th>
2024-12-09 15:14:00 +0700

    reponsive news in homepage

--- components/TagLine.vue
+++ components/TagLine.vue
@@ -1,30 +1,22 @@
 <template>
     <div class="image-container">
         <img src="~/assets/img/tl.png" class="img" alt="Coconut Image">
-        <h1>This is Coconut Knowledge Hub</h1>
+        <h1>{{taglinetext}}</h1>
     </div>
 </template>
 
-<style>
-.image-container {
-    position: relative; /* Allows positioning of h1 relative to this container */
-    width: 100%;
-    height: auto;
-}
 
-h1 {
-    text-shadow: 2px 2px 2px rgba(0, 0, 0, 1);
-    color: white;
-    position: absolute;
-    top: 50%;  
-    left: 50%; 
-    transform: translate(-50%, -50%); 
-    font-size: 2em; 
-    margin: 0; 
-}
 
-.img {
-    width: 100%;
-    height: auto;
+<script>
+import '~/components/styles/tagline.css';
+export default {
+    props: {
+        taglinetext:
+        {
+            type: String,
+            required: true
+        }
+
+    }
 }
-</style>
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,17 @@
 <template>
     <Navbar selecto="home" />
-    <TagLine />
+    <TagLine taglinetext="bruh, dis is Coconut Knowledge Hub "/>
+    <ContentHeader contexto="ข่าวสาร" />
+    <sda newsimage="https://static.bangkokpost.com/media/content/dcx/2022/06/06/4326870_790.jpg " newstitle="  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit eaque sint molestias qui non sunt facere, earum nesciunt doloremque ipsa nemo molestiae iusto libero est facilis, quia perferendis delectus enim?" newsid="w" />   
+    <ContentHeader contexto="กิจกรรม" />
+    
     <Footer />
   </template>
+  <!--ตรงนี้ใช้ลอง footerอย่าลืมเอาออก -->
   <style scoped>
   Footer{
     margin-top: 100%;
   }
   
 </style>
+
