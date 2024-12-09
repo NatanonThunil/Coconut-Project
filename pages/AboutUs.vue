25a2acdf54eb1fe718c3c34270cca2c06277ba84
JeerasakSampeng <6531503016@lamduan.mfu.ac.th>
2024-12-09 22:59:14 +0700

    test commit

--- pages/AboutUs.vue
+++ pages/AboutUs.vue
@@ -1,7 +1,7 @@
 <template>
   <Navbar selecto="aboutus"/>
     <section>
-      <p>This page will be displayed at the /about route.</p>
+      <p>This page will be displayed at the /about route. test</p>
     </section>
   </template>
   
