d5cfae4554d22dad018b229d2d6f952f51f7731a
WasitpolKuekkong <6531503072@lamduan.mfu.ac.th>
2024-12-24 16:29:23 +0700

    more reponsive ui

--- server/server.js
+++ server/server.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const newsApi = require('./api/news_table'); // Import the new API file
-const eventsApi = require('./api/events_table');
+// const express = require('express');
+// const cors = require('cors');
+// const newsApi = require('./api/news_table'); // Import the new API file
+// const eventsApi = require('./api/events_table');
 
-const app = express();
-const port = 3000;
+// const app = express();
+// const port = 3000;
 
-// Middleware
-app.use(express.json());
-app.use(cors());
+// // Middleware
+// app.use(express.json());
+// app.use(cors());
 
-// Use the newsApi router
-// app.use('/api/new', newsApi)
- app.use('/api/event', eventsApi);
+// // Use the newsApi router
+// // app.use('/api/new', newsApi)
+//  app.use('/api/event', eventsApi);
 
 
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
