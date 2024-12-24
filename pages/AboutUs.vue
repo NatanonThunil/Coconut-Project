cbbb654f485a2162fe4f10c10948e7d415e291a4
WasitpolKuekkong <6531503072@lamduan.mfu.ac.th>
2024-12-24 19:20:29 +0700

    merge with jame

--- pages/AboutUs.vue
+++ pages/AboutUs.vue
@@ -13,8 +13,11 @@
         <div class="headers"><h1></h1></div>
         <div class="box1">
           <div class="greenbox1">
+            <div class="textcontent">
+          
             <div class="content1">ผลงาน</div>
             <div class="text">ข้อความ</div>
+          </div>
             <div class="image">
               <img
                 src="https://static.cdntap.com/tap-assets-prod/wp-content/uploads/sites/25/2020/04/32-%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%95%E0%B8%B9%E0%B8%99%E0%B9%80%E0%B8%94%E0%B9%87%E0%B8%8134.png"
@@ -23,8 +26,11 @@
             </div>
           </div>
           <div class="greenbox2">
+            <div class="textcontent">
+          
             <div class="content1">ผลงาน</div>
             <div class="text">ข้อความ</div>
+          </div>
             <div class="image">
               <img
                 src="https://static.cdntap.com/tap-assets-prod/wp-content/uploads/sites/25/2020/04/32-%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%95%E0%B8%B9%E0%B8%99%E0%B9%80%E0%B8%94%E0%B9%87%E0%B8%8134.png"
@@ -42,59 +48,104 @@
   </div>
   <ContentHeader class="ch" contexto="คณะทำงาน" />
   <div class="cards-container">
-    <Card title="ชื่อ" image="https://pbs.twimg.com/media/EvMY9LqVkAMfW-9.jpg" description="ข้อความ"></Card>
-    <Card title="ชื่อ" image="https://s359.kapook.com/r/600/auto/pagebuilder/19c270cb-85d2-4682-956a-8151861d0936.jpg" description="ข้อความ"></Card>
-    
+    <Card
+      title="ชื่อ"
+      image="https://pbs.twimg.com/media/EvMY9LqVkAMfW-9.jpg"
+      description="ข้อความ"
+    ></Card>
+    <Card
+      title="ชื่อ"
+      image="https://s359.kapook.com/r/600/auto/pagebuilder/19c270cb-85d2-4682-956a-8151861d0936.jpg"
+      description="ข้อความ"
+    ></Card>
+    <Card
+      title="ชื่อ"
+      image="https://pbs.twimg.com/media/EvMY9LqVkAMfW-9.jpg"
+      description="ข้อความ"
+    ></Card>
   </div>
-  
 
   <ContentHeader class="ch" contexto="สิทธิประโยชน์และการบริการ" />
+  <div class="cards-container">
+    <Card
+      title="ชื่อ"
+      image="https://pbs.twimg.com/media/EvMY9LqVkAMfW-9.jpg"
+      description="ข้อความ"
+    ></Card>
+    <Card
+      title="ชื่อ"
+      image="https://s359.kapook.com/r/600/auto/pagebuilder/19c270cb-85d2-4682-956a-8151861d0936.jpg"
+      description="ข้อความ"
+    ></Card>
+    <Card
+      title="ชื่อ"
+      image="https://pbs.twimg.com/media/EvMY9LqVkAMfW-9.jpg"
+      description="ข้อความ"
+    ></Card>
+  </div>
 
   <ContentHeader class="ch" contexto="สมาชิก" />
-  <Footer/>
+  <div class="cards-container">
+    <Card
+      title="ชื่อ"
+      image="https://pbs.twimg.com/media/EvMY9LqVkAMfW-9.jpg"
+      description="ข้อความ"
+    ></Card>
+    <Card
+      title="ชื่อ"
+      image="https://s359.kapook.com/r/600/auto/pagebuilder/19c270cb-85d2-4682-956a-8151861d0936.jpg"
+      description="ข้อความ"
+    ></Card>
+    <Card
+      title="ชื่อ"
+      image="https://pbs.twimg.com/media/EvMY9LqVkAMfW-9.jpg"
+      description="ข้อความ"
+    ></Card>
+  </div>
+  <Footer />
 </template>
 
 <style scoped>
 .navcontainer {
   width: 100vw;
   height: 100px;
-  border: green solid 2px;
 }
 .a {
   padding-top: 20px;
-  border: 2px solid #000;
 }
 .headers h1 {
   text-align: center;
 }
 .box1 {
-  border: 2px solid #ff0000;
   display: flex;
   flex-direction: column;
   align-items: center;
 }
-.greenbox1 {
+.greenbox1,.greenbox2 {
   margin-bottom: 53px;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   background-color: #c5d944;
-}
-.greenbox1,
-.greenbox2 {
+  
   width: 1450px;
   height: 347px;
-  border: 2px solid #004010;
-  background-color: #c5d944;
+}
+.textcontent{
+  flex: 0 0 40%;
+}
+.image{
+  flex: 0 0 60%;
+  display:flex;
+  justify-content: center;
+  align-items: center;
 }
 .image img {
   height: 347px;
+  object-fit: cover;
+  width: 100%;
 }
-.greenbox2 {
-  display: flex;
-  justify-content: space-between;
-}
+
 .header-container {
-  border: blue solid 3px;
   text-align: center;
   margin-top: 0;
   padding-top: 20px;
@@ -107,14 +158,14 @@
 }
 .content1 {
   color: #000;
-  margin-left: 3%;
-  margin-top: 2%;
+  margin-left: 10%;
+  margin-top: 10px;
   font-weight: 700;
   font-size: 40px;
 }
 .text {
-  margin-left: -45%;
-  margin-top: 8%;
+  margin-left: 15%;
+  margin-top: 7px;
   font-size: 36px;
   font-weight: 400;
 }
@@ -136,7 +187,7 @@
   border-radius: 20;
 }
 .ch {
-  margin-left: 10px;
+  margin-left: 100px;
 }
 .card-image img {
   display: block; /* เปลี่ยนเป็น block ให้รูปครอบคลุมเต็ม */
@@ -145,7 +196,6 @@
   object-fit: cover; /* ให้รูปภาพครอบคลุม */
   border-radius: 10px 10px 0 0; /* มุมโค้งด้านบน */
   flex-direction: row;
-  
 }
 
 .card-text {
@@ -168,7 +218,7 @@
   width: 400px;
   height: 512px;
   background-color: #ffffff;
-  margin-left: 250px;
+  /* margin-left: 250px; */
   border-radius: 10px; /* มุมโค้งทุกด้าน */
   border: 2px solid #000000; /* กำหนดเส้นขอบ */
   overflow: hidden; /* ซ่อนส่วนเกิน */
@@ -176,13 +226,14 @@
 }
 
 .cards-container {
+  justify-self: center;
   display: flex;
   flex-direction: row; /* Stack cards vertically */
   gap: 20px; /* Adds spacing between the cards */
   justify-content: flex-start; /* Centers cards vertically in their container */
   align-items: center; /* Centers cards horizontally in their container */
   margin-top: 20px;
-  flex-wrap: wrap;/* การ์ดจะลงแถวใหม่ถ้าพื้นที่ไม่พอ */
+  flex-wrap: wrap; /* การ์ดจะลงแถวใหม่ถ้าพื้นที่ไม่พอ */
 }
 .footer {
   margin-top: 2rem;
